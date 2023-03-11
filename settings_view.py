7372966e896aefc6b94c003a74470809ae6f8612
tamir303 <tamir303@gmail.com>
2023-03-11 13:56:10 +0200

    fixed some issues in game_view and added new twicks

--- settings_view.py
+++ settings_view.py
@@ -2,9 +2,11 @@ import tkinter as tk
 from tkinter import font
 import prisoners_view
 import threading
+
+
 class settings_view:
     def __init__(self, x_size, y_size, controller, main_view):
-           # Main game settings
+        # Main game settings
         self.controller = controller
         self.main_view = main_view
         self.root = tk.Tk()
@@ -13,29 +15,38 @@ class settings_view:
         self.screenwidth = self.root.winfo_screenwidth()
         self.screenheight = self.root.winfo_screenheight()
         self.settings_screen = tk.Frame(self.root, bg='#f5deb3')
-        alignstr = '%dx%d+%d+%d' % (x_size, y_size, (self.screenwidth - x_size) / 2, (self.screenheight - y_size) / 2)
+        alignstr = '%dx%d+%d+%d' % (x_size, y_size, (self.screenwidth -
+                                    x_size) / 2, (self.screenheight - y_size) / 2)
         self.root.geometry(alignstr)
         self.root.configure(bg='#F5DEB3')  # Set light brown background
         self.game = 0
         # Buttons, Labels, TextInput ...
         font_style = font.Font(family='Arial', size=12, weight='bold')
-        button_style = {'bg': '#8FBC8F', 'fg': 'white', 'font': font_style, 'activebackground': '#90EE90'}
+        button_style = {'bg': '#8FBC8F', 'fg': 'white',
+                        'font': font_style, 'activebackground': '#90EE90'}
 
-        self.startLabel = tk.Label(text="Press to start the game", font=font_style, bg='#F5DEB3')
-        self.playButton = tk.Button(self.root, text='Play', command=lambda: self.play(), **button_style)
-        
-        self.simulationLabel = tk.Label(text="Open Game Simulation", font=font_style, bg='#F5DEB3')
-        self.playSimulationButton = tk.Button(self.root, text='PlaySimulation', command=lambda: self.play_simulation(), **button_style)
+        self.startLabel = tk.Label(
+            text="Press to start the game", font=font_style, bg='#F5DEB3')
+        self.playButton = tk.Button(
+            self.root, text='Play', command=lambda: self.play(), **button_style)
 
-        self.prisonerstLabel = tk.Label(text="Enter amount of prisoners", font=font_style, bg='#F5DEB3')
+        self.simulationLabel = tk.Label(
+            text="Open Game Simulation", font=font_style, bg='#F5DEB3')
+        self.playSimulationButton = tk.Button(
+            self.root, text='PlaySimulation', command=lambda: self.play_simulation(), **button_style)
+
+        self.prisonerstLabel = tk.Label(
+            text="Enter amount of prisoners", font=font_style, bg='#F5DEB3')
         self.prisonersInput = tk.Scale(self.root, from_=2, to=1000, orient=tk.HORIZONTAL, length=400,
                                        font=font_style, bg='#F5DEB3')
 
-        self.gamesLabel = tk.Label(text="Enter number of games", font=font_style, bg='#F5DEB3')
+        self.gamesLabel = tk.Label(
+            text="Enter number of games", font=font_style, bg='#F5DEB3')
         self.gamesInput = tk.Scale(self.root, from_=1, to=100, orient=tk.HORIZONTAL, length=400,
-                                    font=font_style, bg='#F5DEB3')
+                                   font=font_style, bg='#F5DEB3')
 
-        self.strategyLabel = tk.Label(text="Choose your strategy", font=font_style, bg='#F5DEB3')
+        self.strategyLabel = tk.Label(
+            text="Choose your strategy", font=font_style, bg='#F5DEB3')
         self.optimized = False
         self.optimizedRadioOn = tk.Radiobutton(self.root, text="Optimized", variable=self.optimized,
                                                value=True, command=lambda: self.change_opt(True),
@@ -43,19 +54,21 @@ class settings_view:
         self.optimizedRadioOff = tk.Radiobutton(self.root, text="Random", variable=self.optimized,
                                                 value=False, command=lambda: self.change_opt(False),
                                                 font=font_style, bg='#F5DEB3', activebackground='#90EE90')
-        
+
         self.packing()
         self.root.mainloop()
 
     def play(self):
         self.__change_game_settings()
         self.controller.start_game(self.optimized)
+
     def play_simulation(self):
-        my_thread = threading.Thread(target= self.main_view.run(self))
+        my_thread = threading.Thread(target=self.main_view.run(self))
         # Start the thread
         my_thread.start()
         # Wait for the thread to finish (optional)
         my_thread.join()
+
     def __change_game_settings(self):
         self.controller.change_model_prisoners(self.prisonersInput.get())
         self.controller.change_model_games(self.gamesInput.get())
--- sprites.py
+++ sprites.py
@@ -1,12 +1,16 @@
 import pygame
 
+ASSETS_FOLDER = 'assets/'
+
+
 class Prisoner(pygame.sprite.Sprite):
 
     IMG = 'prisoner.png'
 
     def __init__(self, pos):
         super(Prisoner, self).__init__()
-        self.image = pygame.image.load('prisoner.png').convert_alpha()
+        self.image = pygame.image.load(
+            ASSETS_FOLDER + Prisoner.IMG).convert_alpha()
         self.image = pygame.transform.smoothscale(self.image, (100, 100))
         self.image = pygame.transform.flip(self.image, 90, 0)
         self.rect = self.image.get_rect(center=pos)
@@ -16,26 +20,30 @@ class Prisoner(pygame.sprite.Sprite):
         self.pos = pos
         self.rect = self.image.get_rect(center=pos)
 
+
 class Box(pygame.sprite.Sprite):
 
     IMG_OPEN = 'box_open.png'
     IMG_CLOSED = 'box_closed.png'
 
-    def __init__(self, pos, number):
+    def __init__(self, pos):
         super(Box, self).__init__()
-        self.image = pygame.image.load(Box.IMG_CLOSED).convert_alpha()
+        self.image = pygame.image.load(
+            ASSETS_FOLDER + Box.IMG_CLOSED).convert_alpha()
         self.image = pygame.transform.smoothscale(self.image, (80, 80))
         self.rect = self.image.get_rect(center=pos)
         self.pos = pos
-        self.number = number
 
     def open_box(self):
-        self.image = pygame.image.load(Box.IMG_OPEN).convert_alpha()
+        self.image = pygame.image.load(
+            ASSETS_FOLDER + Box.IMG_OPEN).convert_alpha()
         self.image = pygame.transform.smoothscale(self.image, (85, 85))
 
     def close_box(self):
-        self.image = pygame.image.load(Box.IMG_CLOSED).convert_alpha()
+        self.image = pygame.image.load(
+            ASSETS_FOLDER + Box.IMG_CLOSED).convert_alpha()
         self.image = pygame.transform.smoothscale(self.image, (80, 80))
+
 
 class Paper(pygame.sprite.Sprite):
 
@@ -43,7 +51,8 @@ class Paper(pygame.sprite.Sprite):
 
     def __init__(self, pos=(0, 0)):
         super(Paper, self).__init__()
-        self.image = pygame.image.load(Box.IMG_CLOSED).convert_alpha()
+        self.image = pygame.image.load(
+            ASSETS_FOLDER + Paper.IMG_PAPER).convert_alpha()
         self.image = pygame.transform.smoothscale(self.image, (40, 40))
         self.rect = self.image.get_rect(center=pos)
         self.pos = pos
